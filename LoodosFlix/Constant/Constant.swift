2fe9c0b14ad5cb6042175afeec4f6217d5cc4458
Celal Tok <celal.tok@portalgrup.com>
2021-05-12 03:11:40 +0300

    create constant

---
+++ LoodosFlix/Constant/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  LoodosFlix
+//
+//  Created by Celal Tok on 12.05.2021.
+//
+
+import Foundation
+
+///Base Constant
+class Constant {}
---
+++ LoodosFlix/Constant/FirebaseConstant/FirebaseConstant.swift
@@ -0,0 +1,14 @@
+//
+//  FirebaseConstant.swift
+//  LoodosFlix
+//
+//  Created by Celal Tok on 12.05.2021.
+//
+
+import Foundation
+
+extension Constant {
+    class FirebaseConsant {
+        static let REMOTE_CONFIG_KEY = "launch_view_title"
+    }
+}
