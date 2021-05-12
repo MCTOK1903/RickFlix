24e7a6d2e8b1310806ee38d1c6f5659bc5b237cc
Celal Tok <celal.tok@portalgrup.com>
2021-05-12 03:11:26 +0300

    code review

--- LoodosFlix/App/AppDelegate.swift
+++ LoodosFlix/App/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-import Network
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,9 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         NetworkMonitor.shared.startMonitoring()
-        
-       
-        
+        FirebaseApp.configure()
         return true
     }
 
--- LoodosFlix/Network/NetworkMonitor.swift
+++ LoodosFlix/Network/NetworkMonitor.swift
@@ -9,6 +9,7 @@ import Foundation
 import Network
 
 class NetworkMonitor {
+    
     static let shared = NetworkMonitor()
     
     private let queue = DispatchQueue.global()
@@ -16,43 +17,23 @@ class NetworkMonitor {
     
     public private(set) var isConnected: Bool = false
     
-    public private(set) var connectionType: ConectionType = .unkown
-    
-    enum ConectionType {
-        case wifi
-        case cellular
-        case ethernet
-        case unkown
-    }
-    
     private init() {
         monitor = NWPathMonitor()
     }
+}
+
+// MARK: - Public Funcs
+
+extension NetworkMonitor {
     
     public func startMonitoring() {
         monitor.start(queue: queue)
         monitor.pathUpdateHandler = { [weak self] path in
             self?.isConnected = path.status == .satisfied
-            self?.getConnectionType(path)
         }
     }
     
     public func stopMonitoring() {
         monitor.cancel()
-    }
-    
-    private func getConnectionType(_ path: NWPath) {
-        if path.usesInterfaceType(.wifi) {
-            connectionType = .wifi
-        }
-        else if path.usesInterfaceType(.cellular) {
-            connectionType = .cellular
-        }
-        else if path.usesInterfaceType(.wiredEthernet) {
-            connectionType = .ethernet
-        }
-        else {
-            connectionType = .unkown
-        }
     }
 }
