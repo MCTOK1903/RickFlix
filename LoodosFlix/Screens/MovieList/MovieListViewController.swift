8b5dd008ae00f5f61975b6502500aa9116b629e3
Celal Tok <celal.tok@portalgrup.com>
2021-05-12 03:12:08 +0300

    create movieList VC

---
+++ LoodosFlix/Screens/MovieList/MovieListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MovieListViewController.swift
+//  LoodosFlix
+//
+//  Created by Celal Tok on 12.05.2021.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
